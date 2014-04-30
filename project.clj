eaa175799663c18559064c435760f8c6697c9941
Austin Haas <austin@pettomato.com>
2014-04-30 10:33:32 -0700

    Remove stuff from defproject that I don't use.

--- project.clj
+++ project.clj
@@ -22,15 +22,11 @@
 
   :hooks [cljx.hooks]
 
-  :cljsbuild {:test-commands {"node" ["node" :node-runner "target/testable.js"]}
-              :builds [{:source-paths ["target/classes" "target/test-classes"]
+  :cljsbuild {:builds [{:source-paths ["target/classes" "target/test-classes"]
                         :compiler {:output-to "target/testable.js"
                                    :optimizations :advanced
                                    :pretty-print true}}]}
 
   :profiles {:dev {:plugins [[org.clojure/clojurescript "0.0-2202"]
                              [com.keminglabs/cljx "0.3.2"]
-                             [lein-cljsbuild "1.0.3"]]
-                   :aliases {"cleantest" ["do" "clean," "cljx" "once," "test,"
-                                          "cljsbuild" "test"]
-                             "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]}}})
+                             [lein-cljsbuild "1.0.3"]]}})
