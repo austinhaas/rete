ac19a9a31a7f5c06435b0f75ac5273708fdf2924
Austin Haas <austin@pettomato.com>
2014-05-27 19:29:08 -0700

    New compiled version. 1st draft.

---
+++ src/clj/com/pettomato/rete/rete_macros.clj
@@ -0,0 +1,366 @@
+(ns com.pettomato.rete.rete-macros
+  (:require
+   [clojure.walk :refer [postwalk]]))
+
+(defn update
+  "Updates the value in map m at k with the function f.
+
+  Like update-in, but for updating a single top-level key.
+  Any additional args will be passed to f after the value."
+  ([m k f] (assoc m k (f (get m k))))
+  ([m k f x1] (assoc m k (f (get m k) x1)))
+  ([m k f x1 x2] (assoc m k (f (get m k) x1 x2)))
+  ([m k f x1 x2 & xs] (assoc m k (apply f (get m k) x1 x2 xs))))
+
+(defn memoize-once
+  [f]
+  (let [mem (atom {})]
+    (fn [& args]
+      (if-let [e (find @mem args)]
+        (do (swap! mem dissoc args)
+            (val e))
+        (let [ret (apply f args)]
+          (swap! mem assoc args ret)
+          ret)))))
+
+(defn var-symbol? [x] (and (symbol? x) (= (get (str x) 0) \?)))
+
+(defn expr? [x] (and (list? x) (symbol? (first x))))
+
+(defn default-inv-match [xs]
+  ;; Flip ops. Reverse the whole thing by using a list.
+  (reduce (fn [acc [op v]] (cons (case op :- [:+ v] :+ [:- v]) acc)) () xs))
+
+(defn mk-prod-fn [preconds achieves deletes cache? inv-match]
+  (let [;; 'var->pos' is a mapping from each variable in preconditions to
+        ;; the position where it first appears.
+        ;;   e.g., [[?a 'x] [?b 'y]] => {?a [0 0], ?b [1 0]}.
+        var->pos  (->> preconds
+                       (map-indexed (fn [ri c] (map-indexed (fn [ci t] [t [ri ci]]) c)))
+                       (apply concat)
+                       (filter (comp var-symbol? first))
+                       reverse    ;; So that earlier bindings will replace later bindings.
+                       (into {}))
+        inv-match (if (true? inv-match)
+                    `default-inv-match
+                    inv-match)
+        match     (gensym)
+        bindings  (atom [])
+        expr->var (atom {})
+        template  (->> (concat (for [x deletes]  [:- x])
+                               (for [x achieves] [:+ x]))
+                       (postwalk #(cond
+                                   (contains? var->pos %) (let [v    %
+                                                                pos  (get var->pos v)
+                                                                expr `(get-in ~match ~pos)]
+                                                            (swap! bindings into [v expr])
+                                                            v)
+                                   (expr? %)              (let [v    (@expr->var % (gensym))
+                                                                expr %]
+                                                            (swap! bindings into [v expr])
+                                                            (swap! expr->var assoc % v)
+                                                            v)
+                                   :else                  %))
+                       vec)
+        bindings  (distinct @bindings)
+        add-fn    `(fn [~match]
+                     (let [~@bindings]
+                       ~template))
+        add-fn    (if cache? `(memoize-once ~add-fn) add-fn)
+        rem-fn    (if inv-match
+                    `(comp ~inv-match ~add-fn)
+                    `(constantly []))]
+    `(fn [matches#]
+       (reduce (fn [v# [op# match#]]
+                 (case op#
+                   :+ (into v# (~add-fn match#))
+                   :- (into v# (~rem-fn match#))))
+               []
+               matches#))))
+
+(defn canonicalize-rule [r]
+  (if (contains? r :achieves)
+    (let [{:keys [preconds achieves deletes cache? inv-match]} r]
+      (-> r
+          (assoc :fn (mk-prod-fn preconds achieves deletes cache? inv-match))
+          (dissoc :achieves :deletes)))
+    r))
+
+(defn condition->alpha-tests [c]
+  (->> (map-indexed #(vector '= %1 %2) c)
+       (remove (comp var-symbol? peek))))
+
+(defn condition->equality-tests [ri c vars]
+  (->> (map-indexed #(vector [ri %1] %2) c)
+       (filter (comp var-symbol? second))
+       (map (fn [[pos v]] (vector '= pos (get vars v))))
+       (remove (fn [[op p1 p2]] (= p1 p2)))))
+
+(defn canonicalize-tests [r]
+  (let [var->pos    (->> r
+                         (map-indexed (fn [ri c] (map-indexed (fn [ci v] [v [ri ci]]) c)))
+                         (apply concat)
+                         (filter (comp var-symbol? first))
+                         reverse
+                         (into {}))
+        alpha-tests (map condition->alpha-tests r)
+        equal-tests (map-indexed #(condition->equality-tests %1 %2 var->pos) r)]
+    (map vector alpha-tests equal-tests)))
+
+(defn build-nodes [rs]
+  (let [rs (map (fn [r] (update r :preconds canonicalize-tests)) rs)
+        dummy {:type   :beta-mem
+               :flag   :dummy
+               :id     []
+               :parent nil
+               :tests  nil}]
+    (loop [rs    rs
+           nodes []]
+      (if (empty? rs)
+        (distinct nodes)
+        (let [[r & rs']     rs
+              [nodes' beta] (loop [cs    (:preconds r)
+                                   nodes nodes
+                                   beta  dummy]
+                              (if (empty? cs)
+                                [nodes beta]
+                                (let [[c & cs'] cs
+                                      [a b]     c ;; a = constant tests, b = consistency tests.
+                                      alphas (loop [tests  a
+                                                    acc    ()]
+                                               (if (empty? tests)
+                                                 acc
+                                                 (let [[t & ts]  tests
+                                                       parent    (vec (:id (first acc)))
+                                                       id        (conj parent t b)
+                                                       test-node {:type   :alpha-test
+                                                                  :parent parent
+                                                                  :id     id
+                                                                  :test   t}
+                                                       mem-node  {:type   :alpha-mem
+                                                                  :parent id
+                                                                  :id     id
+                                                                  :tests  b}
+                                                       acc'     (conj acc test-node mem-node)]
+                                                   (recur ts acc'))))
+                                      beta'  (if (empty? b)
+                                               beta
+                                               (dissoc (assoc beta :tests b :id (conj (:id beta) b)) :flag))
+                                      join   (let [alpha-id (:id (first alphas))
+                                                   beta-id  (:id beta')]
+                                               {:type  :join
+                                                :id    (conj beta-id c)
+                                                :alpha alpha-id
+                                                :beta  beta-id
+                                                :tests b})
+                                      b-out  {:type   :beta-mem
+                                              :parent (:id join)
+                                              :id     (:id join)
+                                              :tests  nil}
+                                      nodes' (reverse (conj alphas beta' join))]
+                                  (recur cs' (into nodes nodes') b-out))))
+              prod          {:type     :production
+                             :parent   (:id beta)
+                             :id       (gensym)
+                             :priority (:priority r)
+                             :preconds (:preconds r)
+                             :fn       (:fn r)}]
+          (recur rs' (conj nodes' beta prod)))))))
+
+(defn index-nodes [nodes]
+  (let [sig->id (zipmap (map (juxt :type :id) nodes) (range))
+        nodes'  (map (fn [n] (assoc n :id (sig->id ((juxt :type :id) n)))) nodes)
+        i-nodes (vec (sort-by :id nodes'))]
+    (mapv (fn [node]
+            (case (:type node)
+              :alpha-test (update node :parent #(get sig->id [:alpha-mem %]))
+              :alpha-mem  (update node :parent #(get sig->id [:alpha-test %]))
+              :beta-mem   (update node :parent #(get sig->id [:join %]))
+              :join       (-> node
+                              (update :alpha #(get sig->id [:alpha-mem %]))
+                              (update :beta  #(get sig->id [:beta-mem %])))
+              :production (update node :parent #(get sig->id [:beta-mem %]))))
+          i-nodes)))
+
+(defn add-successor-edges [nodes]
+  (reduce (fn [ns n]
+            (case (:type n)
+              :join (-> ns
+                        (update-in [(:alpha n) :successors] (fnil conj #{}) (:id n))
+                        (update-in [(:beta n)  :successors] (fnil conj #{}) (:id n)))
+              (if (:parent n)
+                (update-in ns [(:parent n) :successors] (fnil conj #{}) (:id n))
+                ns)))
+          nodes
+          nodes))
+
+(defn add-key-fns [nodes]
+  (mapv (fn [n]
+          (case (:type n)
+            :alpha-test n
+            :alpha-mem  (let [id     (:id n)
+                              tests  (:tests n)
+                              ps     (mapv #(get-in % [1 1]) tests)
+                              key-fn `(fn alpha-mem-key-fn [w#]
+                                        (list* :alpha-mem ~id (map #(get w# %) ~ps)))]
+                          (assoc n :key-fn key-fn))
+            :beta-mem   (let [id     (:id n)
+                              tests  (:tests n)
+                              ps     (mapv #(get % 2) tests)
+                              key-fn `(fn beta-mem-key-fn [t#]
+                                        (list* :beta-mem ~id (map #(get-in t# %) ~ps)))]
+                          (assoc n :key-fn key-fn))
+            :join       (let [id           (:id n)
+                              tests        (:tests n)
+                              alpha-id     (:alpha n)
+                              beta-id      (:beta n)
+                              ps1          (mapv #(get % 2) tests)
+                              alpha-key-fn `(fn join-alpha-key-fn [t#]
+                                              (list* :alpha-mem ~alpha-id (map #(get-in t# %) ~ps1)))
+                              ps2          (mapv #(get-in % [1 1]) tests)
+                              beta-key-fn  `(fn join-beta-key-fn [w#]
+                                              (list* :beta-mem ~beta-id (map #(get w# %) ~ps2)))]
+                          (assoc n :alpha-key-fn alpha-key-fn :beta-key-fn beta-key-fn))
+            :production n))
+        nodes))
+
+(defn compile-left-activation [nodes R-sym k ts-sym]
+  (let [n (nodes k)]
+    (case (:type n)
+      :beta-mem
+      (let [{:keys [key-fn successors]} n]
+        `(let [~R-sym (reduce #(update-in %1 (~key-fn %2) (fnil conj #{}) %2) ~R-sym ~ts-sym)]
+           (let [~@(interleave (repeat R-sym) (map #(compile-left-activation nodes R-sym % ts-sym) successors))]
+             ~R-sym)))
+      :join
+      (let [{:keys [alpha-key-fn successors]} n]
+        `(let [~ts-sym (for [t# ~ts-sym, w# (get-in ~R-sym (~alpha-key-fn t#))] (conj t# w#))]
+           (if (empty? ~ts-sym)
+             ~R-sym
+             (let [~@(interleave (repeat R-sym) (map #(compile-left-activation nodes R-sym % ts-sym) successors))]
+               ~R-sym))))
+      :production
+      (let [{:keys [id priority]} n]
+        `(-> ~R-sym
+             (update-in [:matches ~id] (fnil into []) (map #(vector :+ %) ~ts-sym))
+             (update-in [:activated-productions] conj [~priority ~id]))))))
+
+(defn compile-right-activation [nodes R-sym k w-sym]
+  (let [n (nodes k)]
+    (case (:type n)
+      :alpha-test
+      (let [{:keys [test successors]} n
+            [op pos val] test]
+        `(if (~op (get ~w-sym ~pos) ~val)
+           (let [~@(interleave (repeat R-sym) (map #(compile-right-activation nodes R-sym % w-sym) successors))]
+             ~R-sym)
+           ~R-sym))
+      :alpha-mem
+      (let [{:keys [key-fn successors]} n]
+        `(let [~R-sym (update-in ~R-sym (~key-fn ~w-sym) (fnil conj #{}) ~w-sym)]
+           (let [~@(interleave (repeat R-sym) (map #(compile-right-activation nodes R-sym % w-sym) successors))]
+             ~R-sym)))
+      :join
+      (let [{:keys [beta-key-fn successors]} n
+            ts (gensym)]
+        `(let [~ts (get-in ~R-sym (~beta-key-fn ~w-sym))
+               ~ts (for [t# ~ts] (conj t# ~w-sym))]
+           (if (empty? ~ts)
+             ~R-sym
+             (let [~@(interleave (repeat R-sym) (map #(compile-left-activation nodes R-sym % ts) successors))]
+               ~R-sym)))))))
+
+(defn compile-left-activation- [nodes R-sym k ts-sym]
+  (let [n (nodes k)]
+    (case (:type n)
+      :beta-mem
+      (let [{:keys [key-fn successors]} n]
+        `(let [~R-sym (reduce #(update-in %1 (~key-fn %2) (fnil disj #{}) %2) ~R-sym ~ts-sym)]
+           (let [~@(interleave (repeat R-sym) (map #(compile-left-activation- nodes R-sym % ts-sym) successors))]
+             ~R-sym)))
+      :join
+      (let [{:keys [alpha-key-fn successors]} n]
+        `(let [~ts-sym (for [t# ~ts-sym, w# (get-in ~R-sym (~alpha-key-fn t#))] (conj t# w#))]
+           (if (empty? ~ts-sym)
+             ~R-sym
+             (let [~@(interleave (repeat R-sym) (map #(compile-left-activation- nodes R-sym % ts-sym) successors))]
+               ~R-sym))))
+      :production
+      (let [{:keys [id priority]} n]
+        `(-> ~R-sym
+             (update-in [:matches ~id] (fnil into []) (map #(vector :- %) ~ts-sym))
+             (update-in [:activated-productions] conj [~priority ~id]))))))
+
+(defn compile-right-activation- [nodes R-sym k w-sym]
+  (let [n (nodes k)]
+    (case (:type n)
+      :alpha-test
+      (let [{:keys [test successors]} n
+            [op pos val] test]
+        `(if (~op (get ~w-sym ~pos) ~val)
+           (let [~@(interleave (repeat R-sym) (map #(compile-right-activation- nodes R-sym % w-sym) successors))]
+             ~R-sym)
+           ~R-sym))
+      :alpha-mem
+      (let [{:keys [key-fn successors]} n]
+        `(let [~R-sym (update-in ~R-sym (~key-fn ~w-sym) (fnil disj #{}) ~w-sym)]
+           (let [~@(interleave (repeat R-sym) (map #(compile-right-activation- nodes R-sym % w-sym) successors))]
+             ~R-sym)))
+      :join
+      (let [{:keys [beta-key-fn successors]} n
+            ts (gensym)]
+        `(let [~ts (get-in ~R-sym (~beta-key-fn ~w-sym))
+               ~ts (for [t# ~ts] (conj t# ~w-sym))]
+           (if (empty? ~ts)
+             ~R-sym
+             (let [~@(interleave (repeat R-sym) (map #(compile-left-activation- nodes R-sym % ts) successors))]
+               ~R-sym)))))))
+
+(defmacro compile-rete [& rules]
+  (let [nodes    (->> rules
+                      (map canonicalize-rule)
+                      build-nodes
+                      index-nodes
+                      add-successor-edges
+                      add-key-fns)
+        productions (into {} (map (juxt :id :fn) (filter #(= (:type %) :production) nodes)))
+        a->k     (->> nodes
+                      (filter #(= (:type %) :alpha-test))
+                      (filter #(nil? (:parent %)))
+                      (reduce (fn [m n]
+                                (let [[op pos val] (:test n)]
+                                  (if (= pos 0)
+                                    (update m val (fnil conj #{}) (:id n))
+                                    m)))
+                              {}))
+        dummy-id (:id (first (filter #(= (:flag %) :dummy) nodes)))
+        R-sym    (gensym)
+        w-sym    (gensym)
+        add-wme  `(fn [~R-sym ~w-sym]
+                    (case (first ~w-sym)
+                      ~@(apply concat
+                               (for [[k ids] a->k]
+                                 [k `(let [~@(interleave (repeat R-sym)
+                                                         (for [id ids, succ (:successors (nodes id))]
+                                                           (compile-right-activation nodes R-sym succ w-sym)))]
+                                       ~R-sym)]))
+                      ~R-sym))
+        rem-wme  `(fn [~R-sym ~w-sym]
+                    (case (first ~w-sym)
+                      ~@(apply concat
+                               (for [[k ids] a->k]
+                                 [k `(let [~@(interleave (repeat R-sym)
+                                                         (for [id ids, succ (:successors (nodes id))]
+                                                           (compile-right-activation- nodes R-sym succ w-sym)))]
+                                       ~R-sym)]))
+                      ~R-sym))]
+    {:alpha-mem {}
+     :beta-mem  {dummy-id #{[]}}
+     :matches   {}
+     :productions productions
+     :activated-productions (sorted-set-by
+                             (fn [x y]
+                               (let [c (compare (first y) (first x))]
+                                 (if (= c 0) (compare x y) c))))
+     :add-wme add-wme
+     :rem-wme rem-wme}))
--- src/cljx/com/pettomato/rete.cljx
+++ src/cljx/com/pettomato/rete.cljx
@@ -1,90 +1,16 @@
-(ns com.pettomato.rete
-  (:require
-   [com.pettomato.rete.util :refer [update]]))
+(ns com.pettomato.rete)
 
 ;; R : A compiled Rete network. {:keys [root-fn nodes alpha-mem beta-mem matches]}.
 ;; w : A Working Memory Element (wme).
 ;; t : A token, which is a seq of working memory elements.
 ;; m : A match, which is a complete token.
-;; k : An indirect reference to a node.
-
-(defn get-node [R k] ((:nodes R) k))
-
-(defn left-activate [R k ts]
-  (let [node (get-node R k)]
-    (case (:type node)
-      :beta-mem   (let [key-fn (:key-fn node)
-                        R'     (reduce #(update-in %1 (key-fn %2) (fnil conj #{}) %2) R ts)]
-                    (reduce #(left-activate %1 %2 ts) R' (:successors node)))
-      :join       (let [key-fn (:alpha-key-fn node)
-                        ts'    (for [t ts, w (get-in R (key-fn t))] (conj t w))]
-                    (cond
-                     (empty? ts') R
-                     :else        (reduce #(left-activate %1 %2 ts') R (:successors node))))
-      :production (let [id (:id node)
-                        priority (:priority node)
-                        ms (map #(vector :+ %) ts)]
-                    (-> (update-in R [:matches id] (fnil into []) ms)
-                        (update-in [:activated-productions] conj [priority id]))))))
-
-(defn right-activate [R k w]
-  (let [node (get-node R k)]
-    (case (:type node)
-      :alpha-test (let [test (:test node)]
-                    (cond
-                     (test w) (reduce #(right-activate %1 %2 w) R (:successors node))
-                     :else    R))
-      :alpha-mem  (let [key-fn (:key-fn node)
-                        R'     (update-in R (key-fn w) (fnil conj #{}) w)]
-                    (reduce #(right-activate %1 %2 w) R' (:successors node)))
-      :join       (let [key-fn (:beta-key-fn node)
-                        ts     (get-in R (key-fn w))
-                        ts'    (for [t ts] (conj t w))]
-                    (cond
-                     (empty? ts') R
-                     :else        (reduce #(left-activate %1 %2 ts') R (:successors node)))))))
-
-(defn left-activate- [R k ts]
-  (let [node (get-node R k)]
-    (case (:type node)
-      :beta-mem   (let [key-fn (:key-fn node)
-                        R'     (reduce #(update-in %1 (key-fn %2) (fnil disj #{}) %2) R ts)]
-                    (reduce #(left-activate- %1 %2 ts) R' (:successors node)))
-      :join       (let [key-fn (:alpha-key-fn node)
-                        ts'    (for [t ts, w (get-in R (key-fn t))] (conj t w))]
-                    (cond
-                     (empty? ts') R
-                     :else        (reduce #(left-activate- %1 %2 ts') R (:successors node))))
-      :production (let [id (:id node)
-                        priority (:priority node)
-                        ms (map #(vector :- %) ts)]
-                    (-> (update-in R [:matches id] (fnil into []) ms)
-                        (update-in [:activated-productions] conj [priority id]))))))
-
-(defn right-activate- [R k w]
-  (let [node (get-node R k)]
-    (case (:type node)
-      :alpha-test (let [test (:test node)]
-                    (cond
-                     (test w) (reduce #(right-activate- %1 %2 w) R (:successors node))
-                     :else    R))
-      :alpha-mem  (let [key-fn (:key-fn node)
-                        R'     (update-in R (key-fn w) (fnil disj #{}) w)]
-                    (reduce #(right-activate- %1 %2 w) R' (:successors node)))
-      :join       (let [key-fn (:beta-key-fn node)
-                        ts     (get-in R (key-fn w))
-                        ts'    (for [t ts] (conj t w))]
-                    (cond
-                     (empty? ts') R
-                     :else        (reduce #(left-activate- %1 %2 ts') R (:successors node)))))))
 
 (defn has-matches? [R]
   (not (empty? (:activated-productions R))))
 
 (defn trigger-next [R]
   (let [[p id] (first (:activated-productions R))
-        node   (get-node R id)
-        f      (:fn node)
+        f      ((:productions R) id)
         ms     (get-in R [:matches id])
         R'     (-> R
                    (update-in [:activated-productions] disj [p id])
@@ -92,16 +18,27 @@
         out    (f ms)]
     [R' out]))
 
-;;; API
-
 (defn add-wme [R w]
-  (let [successor-fn (:root-fn R)]
-    (if-let [ks (successor-fn w)]
-      (reduce #(right-activate %1 %2 w) R ks)
-      R)))
+  ((:add-wme R) R w))
 
 (defn remove-wme [R w]
-  (let [successor-fn (:root-fn R)]
-    (if-let [ks (successor-fn w)]
-      (reduce #(right-activate- %1 %2 w) R ks)
-      R)))
+  ((:rem-wme R) R w))
+
+(defn apply-op [R [op w]]
+  (case op
+    :+ (add-wme R w)
+    :- (remove-wme R w)))
+
+(defn add-until-stable
+  ([R ops] (add-until-stable R ops 100))
+  ([R ops max-iterations]
+     (loop [R      R
+            ops    (seq ops)
+            acc    (vec ops)
+            safety 0]
+       (assert (< safety max-iterations) (str "safety:" safety))
+       (let [R' (reduce apply-op R ops)]
+         (if (has-matches? R')
+           (let [[R'' out] (trigger-next R')]
+             (recur R'' out (into acc out) (inc safety)))
+           [R' acc])))))
