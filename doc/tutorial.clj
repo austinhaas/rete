7dcb67841a2d9d0db8ccc10bf7b10f5c46ac5fbd
Austin Haas <austin@pettomato.com>
2016-11-05 20:44:54 -0700

    tutorial: improved

--- doc/tutorial.clj
+++ doc/tutorial.clj
@@ -10,23 +10,26 @@
                        :achieves [[:c]]
                        :deletes  [[:d]]}))
 
+
+(has-matches? R) ;; => false
+
 ;; No matches because no facts have been added to the system.
-(has-matches? R)
 
 ;; Pass it some facts.
 (def R' (-> R
             (add-wme [:a])
             (add-wme [:b])))
 
-;; Now there are matches.
-(has-matches? R')
+(has-matches? R') ;; => true
 
 ;; Trigger the next rule that is ready to fire (i.e., has matches).
 (let [[R'' out] (trigger-next R')]
-     out)
+  out)
+
+;; => [[:- [:d]] [:+ [:c]]]
 
 ;; The facts in the output are wrapped with a polarity tag indicating
-;; whether they were added, :+, or removed :-.
+;; whether they were added, :+, or removed, :-.
 
 
 ;;; Multiple rules.
@@ -45,6 +48,8 @@
       [R' out] (add-until-stable R [[:+ [:a]] [:+ [:b]]])]
   out)
 
+;; => [[:+ [:c]] [:+ [:d]] [:+ [:e]]]
+
 
 ;;; Facts are more interesting when they have arguments.
 
@@ -54,6 +59,8 @@
                         :achieves [[:c ?x ?y ?z]]})
       [R' out] (add-until-stable R [[:+ [:a 1 2]] [:+ [:b 3]] [:+ [:b 4]]])]
   out)
+
+;; => [[:+ [:c 1 2 3]] [:+ [:c 1 2 4]]]
 
 ;; Two facts are produced because the :a term is able to match with
 ;; two different :b terms.
@@ -69,6 +76,8 @@
       [R' out] (add-until-stable R [[:+ [:a 1]] [:+ [:b 1]] [:+ [:b 2]]])]
   out)
 
+;; => [[:+ [:c 1]]]
+
 ;; Note that [:b 2] didn't match because there is no [:a 2] fact.
 
 
@@ -76,8 +85,10 @@
 
 (let [R (compile-rules {:preconds [[:a ?a] [:b ?b]]
                         :achieves [[:c (+ ?a ?b)]]})
-      [R' out] (add-until-stable R [[:+ [:a 1]] [:+ [:b 1]] [:+ [:b 2]]])]
+      [R' out] (add-until-stable R [[:+ [:a 1]] [:+ [:b 2]] [:+ [:b 3]]])]
   out)
+
+;; => [[:+ [:c 3]] [:+ [:c 4]]]
 
 
 ;;; Truth maintenance.
@@ -89,11 +100,13 @@
                         :achieves  [[:c (* ?a ?b)]]
                         :inv-match true}) ;; Change to false and eval again.
       [R' out] (add-until-stable R [[:+ [:a 3]] [:+ [:a 4]] [:+ [:b 5]]])]
-  (println "out: " out)
+  (println out)
   ;; Now remove one of the matching elements.
   (let [[R'' out] (add-until-stable R' [[:- [:a 3]]])]
-    (println "out: " out)))
+    (println out)))
 
+;; => [[:+ [:c 20]] [:+ [:c 15]]]
+;; => [[:- [:c 15]]]
 
 ;;; Collapsing matches.
 
@@ -101,6 +114,12 @@
 ;; the same value, but opposite polarity will cancel out. If
 ;; collapse-matches? is false, or not supplied, then both values will
 ;; trigger the rule.
+
+;; Here we have two rules that produce values that should cancel
+;; out. The third rule depends on these values. The default behavior
+;; is to pass everything through without modification. By changing
+;; collapse-matches? to true, we can make these values cancel out
+;; before the production is triggered.
 
 (let [R (compile-rules {:preconds  [[:a ?a] [:b ?b]]
                         :achieves  [[:c (* ?a ?b)]]}
@@ -111,7 +130,13 @@
                         :inv-match true
                         :collapse-matches? false}) ;; Change to true and eval again.
       [R' out] (add-until-stable R [[:+ [:a 3]] [:+ [:b 5]]])]
-  (println "out: " out))
+  out)
+
+;; => [[:+ [:c 15]] [:- [:c 15]] [:+ [:d 15]] [:- [:d 15]]]
+
+;; After changing :collapse-matches? to true:
+
+;; => [[:+ [:c 15]] [:- [:c 15]]]
 
 
 ;;; Rule priority.
@@ -131,15 +156,17 @@
                        {:preconds  [[:a ?a]]
                         :achieves  [[:fourth ?a]]})
       [R' out] (add-until-stable R [[:+ [:a 3]]])]
-  (println "out: " out))
+  out)
+
+;; => [[:+ [:first 3]] [:+ [:second 3]] [:+ [:third 3]] [:+ [:fourth 3]]]
 
 
 ;;; Caching (to speed up truth maintenance).
 
 ;; If cache? is true, then the rule will be memoized with a special
-;; memoization function that only remembers a result one time. In
-;; other words, the second time the production is called with the same
-;; input, it'll return the cached value, but then immediately forget
+;; memoization function that only remembers a result once. In other
+;; words, the second time the production is called with the same
+;; input, it'll return the cached value, and then immediately forget
 ;; it. This is an optimization to avoid performing the calculation
 ;; again when removing a match.
 
@@ -149,8 +176,13 @@
                         :cache?    true}) ;; Change to false and eval again.
       [R' out] (time (add-until-stable R [[:+ [:a 3]]]))
       [R'' out'] (time (add-until-stable R [[:- [:a 3]]]))]
-  (println "out: " out)
-  (println "out': " out'))
+  (println out)
+  (println out'))
+
+;; => "Elapsed time: 1002.107053 msecs"
+;; => "Elapsed time: 2.902768 msecs"
+;; => [[:+ [:b 3]]]
+;; => [[:- [:b 3]]]
 
 
 ;;; Longhand rules.
@@ -159,21 +191,24 @@
 ;; productions. For complete control, the production rule can be
 ;; supplied. This is needed when the set of output fact types isn't
 ;; static.
-(let [R (compile-rules {:preconds  [[:a ?a] [:b ?b]]
-                        :fn        (fn [matches]
-                                     (reduce (fn [acc [op match]]
-                                               (conj acc
-                                                     (let [[[_ a] [_ b]] match
-                                                           result (+ a b)
-                                                           type   (if (odd? result)
-                                                                    :odd
-                                                                    :even)]
-                                                       (case op
-                                                         :+ [:+ [type result]]
-                                                         :- [:- [type result]]))))
-                                             []
-                                             matches))})
+(let [R (compile-rules {:preconds [[:a ?a] [:b ?b]]
+                        :fn       (fn [matches]
+                                    (reduce (fn [acc [op match]]
+                                              (conj acc
+                                                    (let [[[_ a] [_ b]] match
+                                                          result (+ a b)
+                                                          type   (if (odd? result)
+                                                                   :odd
+                                                                   :even)]
+                                                      (case op
+                                                        :+ [:+ [type result]]
+                                                        :- [:- [type result]]))))
+                                            []
+                                            matches))})
       [R' out] (add-until-stable R [[:+ [:a 1]] [:+ [:b 2]] [:+ [:a 2]]])]
-  (println "out: " out)
+  (println out)
   (let [[R'' out] (add-until-stable R' [[:- [:a 1]]])]
-    (println "out: " out)))
+    (println out)))
+
+;; => [[:+ [:odd 3]] [:+ [:even 4]]]
+;; => [[:- [:odd 3]]]
